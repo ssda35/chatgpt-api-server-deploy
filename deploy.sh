9f3a75a68eac00484cd71840b955aab4aa81d965
ssda35 <64445010+ssda35@users.noreply.github.com>
2024-07-20 01:11:42 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,9 @@
 
 set -e
 
-# Di chuyển vào thư mục /app nơi tệp docker-compose.yml hiện diện
-cd /app
-#!/bin/bash
-
-set -e
-
-# Di chuyển vào thư mục /app nơi tệp docker-compose.yml hiện diện
-cd /app
-
 # Kiểm tra kết nối tới Docker daemon
 docker info > /dev/null 2>&1
 
-# Chạy Docker Compose từ thư mục này
-docker-compose pull
-docker-compose up -d --remove-orphans
-# Kiểm tra kết nối tới Docker daemon
-docker info > /dev/null 2>&1
-
-# Chạy Docker Compose từ thư mục này
+# Chạy Docker Compose
 docker-compose pull
 docker-compose up -d --remove-orphans
