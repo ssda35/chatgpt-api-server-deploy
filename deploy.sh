5b4049e65ae5963dd9e6c6a40947e96bbba25a63
ssda35 <64445010+ssda35@users.noreply.github.com>
2024-07-20 00:50:02 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-docker compose pull
-docker compose up -d --remove-orphans
+# Di chuyển vào thư mục /app nơi tệp docker-compose.yml hiện diện
+cd /app
+
+# Chạy Docker Compose từ thư mục này
+docker-compose pull
+docker-compose up -d --remove-orphans
